490a523f13a64c9a75854c936ea4aded20b0cdb2
emeksdot <emeksdot@yahoo.com>
2020-08-11 00:22:17 +0100

    Version 3 methods created

--- script.js
+++ script.js
@@ -56,5 +56,23 @@ function deleteTodo(position){
 // It should have a delete todo method
 
 var todoList = {
-    todos : ['item 1', 'item 2', 'item 3']
+    todos : ['item 1', 'item 2', 'item 3'],
+    displayTodos: function (){
+        console.log('My todos: ', this.todos);
+    },
+
+    addTodo: function (todo) {
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+
+    changeTodo: function (position, newValue){
+        this.todos[position] = newValue;
+        this.displayTodos();
+    }
+    
+    deleteTodo: function (position){
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    }
 }
